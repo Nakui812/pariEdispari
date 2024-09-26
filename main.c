f9cb3af0be7f8c6098ba59496642157d816d616e
nabba <beldes710@gmail.com>
2024-09-26 18:58:15 +0200

    Initial commit

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(void)
+{
+  int a;
+  printf("inserisci un numero \n");
+        scanf("%d", &a);
+   if (a%2 ==0)
+  {
+      printf("il tuo numero e' pari");
+  } else {
+      printf("il tuo numero e' dispari");
+}
+  return 0; }
+
